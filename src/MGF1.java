8af65b1380c61f20ceb2aea3fd6dc3929eb6d7aa
valdir <valdir@AirValdir.local>
2024-04-14 11:14:23 -0300

    refatorar mgf1

--- src/MGF1.java
+++ src/MGF1.java
@@ -5,37 +5,47 @@ public class MGF1 {
 
     private final MessageDigest md;
 
-    /**
-     * Construct an instance of MGF1 based on the specified digest algorithm.
-     */
     MGF1(String mdAlgo) throws NoSuchAlgorithmException {
         this.md = MessageDigest.getInstance(mdAlgo);
     }
 
-    void generateAndXor(byte[] seed, int seedOfs, int seedLen, int maskLen,
-                        byte[] out, int outOfs) throws RuntimeException {
-        byte[] C = new byte[4]; // 32 bit counter
-        byte[] digest = new byte[md.getDigestLength()];
+    public void generateAndXor(byte[] seed, int seedOfs, int seedLen, int maskLen,
+                               byte[] out, int outOfs) {
+        byte[] counter = new byte[4];
+        byte[] digest;
+
         while (maskLen > 0) {
-            md.update(seed, seedOfs, seedLen);
-            md.update(C);
-            try {
-                md.digest(digest, 0, digest.length);
-            } catch (DigestException e) {
-                // should never happen
-                throw new RuntimeException(e.toString());
+            updateMessageDigest(seed, seedOfs, seedLen, counter);
+            digest = calculateDigest();
+
+            for (int i = 0; i < digest.length && maskLen > 0; i++) {
+                out[outOfs++] ^= digest[i];
+                maskLen--;
             }
-            for (int i = 0; (i < digest.length) && (maskLen > 0); maskLen--) {
-                out[outOfs++] ^= digest[i++];
-            }
+
             if (maskLen > 0) {
-                // increment counter
-                for (int i = C.length - 1; (++C[i] == 0) && (i > 0); i--) {
-                    // empty
-                }
+                incrementCounter(counter);
             }
         }
     }
+
+    private void updateMessageDigest(byte[] seed, int seedOfs, int seedLen, byte[] counter) {
+        md.update(seed, seedOfs, seedLen);
+        md.update(counter);
+    }
+
+    private byte[] calculateDigest() {
+        return md.digest();
+    }
+
+    private void incrementCounter(byte[] counter) {
+        for (int i = counter.length - 1; i >= 0; i--) {
+            if (++counter[i] != 0) {
+                break;
+            }
+        }
+    }
+
 
     /**
      * Returns the name of this MGF1 instance, i.e. "MGF1" followed by the
